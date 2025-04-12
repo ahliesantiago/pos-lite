4ebb1417aa05c634b5a4b6c1b5c0a32fa5162cf7
Ahlie Santiago <83854486+ahliesantiago@users.noreply.github.com>
2025-04-13 00:35:34 +0800

    [build fix]: deployment t/s ongoing

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-echo "=== Starting deploy.sh script execution ==="
+set -e
+
+echo ">>> Starting deploy.sh script execution <<<"
 
 # Clear and rebuild Laravel caches
-echo "=== Clearing and rebuilding Laravel caches ==="
+echo ">>> Clearing and rebuilding Laravel caches <<<"
 php artisan config:clear
 php artisan route:clear
 php artisan view:clear
+
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
@@ -15,6 +18,7 @@ chown -R www-data:www-data storage bootstrap/cache
 chmod -R 775 storage bootstrap/cache
 
 # Run migrations
-echo "=== Running migrations ==="
+echo ">>> Running migrations <<<"
 php artisan migrate --force
 php artisan db:seed --force
+php artisan migrate:status
